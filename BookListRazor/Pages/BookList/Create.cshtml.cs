37d9ea73c8a7f6938103f5169b9b5f8c46ab96f7
Ratul Das <ratuldasanik@outlook.com>
2020-11-09 22:02:09 +0600

    first commit

---
+++ BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [BindProperty]//becuse we will be using the same Book object in our post handler
+        public Book Book { get; set; }
+        public void OnGet()
+        {
+        }
+        //you have to specify the type of the task as IActionResult
+        //because we will be redirecting our user to a new page
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                await _db.Book.AddAsync(Book);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+    }
+}
